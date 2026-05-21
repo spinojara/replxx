spinojara/replxx

commit 2a70695bcb42fed385f601a818c6841eff94e673

Support for custom descriptors (#26)

include/replxx.h
@@ -224,7 +224,7 @@ typedef struct ReplxxHistoryEntryTag {
  *
  * \return Replxx library resource holder.
  */
-REPLXX_IMPEXP Replxx* replxx_init( void );
+// REPLXX_IMPEXP Replxx* replxx_init( void );
 
 /*! \brief Cleanup resources used by Replxx library.
  *

src/replxx.cxx
@@ -126,8 +126,8 @@ void delete_ReplxxImpl( Replxx::ReplxxImpl* impl_ ) {
 }
 }
 
-Replxx::Replxx( void )
-	: _impl( new Replxx::ReplxxImpl( nullptr, nullptr, nullptr ), delete_ReplxxImpl ) {
+Replxx::Replxx( std::istream & input_stream_, std::ostream & output_stream_, int in_fd_, int out_fd_, int err_fd_ )
+	: _impl( new Replxx::ReplxxImpl( input_stream_, output_stream_, in_fd_, out_fd_, err_fd_ ), delete_ReplxxImpl ) {
 }
 
 void Replxx::set_completion_callback( completion_callback_t const& fn ) {
@@ -339,10 +339,10 @@ Replxx::Color rgb666( int red_, int green_, int blue_ ) {
 
 }
 
-::Replxx* replxx_init() {
-	typedef ::Replxx* replxx_data_t;
-	return ( reinterpret_cast<replxx_data_t>( new replxx::Replxx::ReplxxImpl( nullptr, nullptr, nullptr ) ) );
-}
+// ::Replxx* replxx_init() {
+// 	typedef ::Replxx* replxx_data_t;
+// 	return ( reinterpret_cast<replxx_data_t>( new replxx::Replxx::ReplxxImpl( nullptr, nullptr, nullptr ) ) );
+// }
 
 void replxx_end( ::Replxx* replxx_ ) {
 	delete reinterpret_cast<replxx::Replxx::ReplxxImpl*>( replxx_ );
@@ -683,7 +683,7 @@ int replxx_history_size( ::Replxx* replxx_ ) {
 void replxx_debug_dump_print_codes(void) {
 	char quit[4];
 
-	printf(
+	dprintf(_out_fd,
 			"replxx key codes debugging mode.\n"
 			"Press keys to see scan codes. Type 'quit' at any time to exit.\n");
 	if (enableRawMode() == -1) return;
@@ -702,9 +702,9 @@ void replxx_debug_dump_print_codes(void) {
 		quit[sizeof(quit) - 1] = c; /* Insert current char on the right. */
 		if (memcmp(quit, "quit", sizeof(quit)) == 0) break;
 
-		printf("'%c' %02x (%d) (type quit to exit)\n", isprint(c) ? c : '?', (int)c,
+		dprintf(_out_fd,"'%c' %02x (%d) (type quit to exit)\n", isprint(c) ? c : '?', (int)c,
 					 (int)c);
-		printf("\r"); /* Go left edge manually, we are in raw mode. */
+		dprintf(_out_fd,"\r"); /* Go left edge manually, we are in raw mode. */
 		fflush(stdout);
 	}
 	disableRawMode();

include/replxx.hxx
@@ -391,7 +391,7 @@ private:
 #endif
 
 public:
-	Replxx( void );
+	Replxx( std::istream & input_stream_, std::ostream & output_stream_, int in_fd_, int out_fd_, int err_fd_ );
 	Replxx( Replxx&& ) = default;
 	Replxx& operator = ( Replxx&& ) = default;
 

src/escape.hxx
@@ -12,7 +12,7 @@ namespace EscapeSequenceProcessing {
 // dispatch routines, then eventually returns the final (possibly extended or
 // special) character.
 //
-typedef char32_t (*CharacterDispatchRoutine)(char32_t);
+typedef char32_t (*CharacterDispatchRoutine)(int, int, char32_t);
 
 // This structure is used by doDispatch() to hold a list of characters to test
 // for and
@@ -27,7 +27,7 @@ struct CharacterDispatch {
 	CharacterDispatchRoutine* dispatch; // array of routines to call
 };
 
-char32_t doDispatch(char32_t c);
+char32_t doDispatch(int in_fd_, int err_fd, char32_t c);
 
 }
 

src/replxx_impl.cxx
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cstdio>
 #include <memory>
 #include <cerrno>
 #include <iostream>
@@ -146,7 +147,7 @@ public:
 
 }
 
-Replxx::ReplxxImpl::ReplxxImpl( FILE*, FILE*, FILE* )
+Replxx::ReplxxImpl::ReplxxImpl( std::istream & in_, std::ostream & out_, int in_fd_, int out_fd_, int err_fd_ )
 	: _utf8Buffer()
 	, _data()
 	, _pos( 0 )
@@ -175,7 +176,7 @@ Replxx::ReplxxImpl::ReplxxImpl( FILE*, FILE*, FILE* )
 	, _indentMultiline( true )
 	, _namedActions()
 	, _keyPressHandlers()
-	, _terminal()
+	, _terminal(in_fd_, out_fd_)
 	, _currentThread()
 	, _prompt( _terminal )
 	, _completionCallback( nullptr )
@@ -200,7 +201,12 @@ Replxx::ReplxxImpl::ReplxxImpl( FILE*, FILE*, FILE* )
 	, _oldPos( 0 )
 	, _moveCursor( false )
 	, _ignoreCase( false )
-	, _mutex() {
+	, _mutex()
+	, _in(in_)
+	, _out(out_)
+	, _in_fd(in_fd_)
+	, _out_fd(out_fd_)
+	, _err_fd(err_fd_) {
 	using namespace std::placeholders;
 	_namedActions[action_names::INSERT_CHARACTER]                  = std::bind( &ReplxxImpl::invoke, this, Replxx::ACTION::INSERT_CHARACTER,                  _1 );
 	_namedActions[action_names::NEW_LINE]                          = std::bind( &ReplxxImpl::invoke, this, Replxx::ACTION::NEW_LINE,                          _1 );
@@ -608,8 +614,8 @@ void Replxx::ReplxxImpl::set_preload_buffer( std::string const& preloadText ) {
 
 char const* Replxx::ReplxxImpl::read_from_stdin( void ) {
 	if ( _preloadedBuffer.empty() ) {
-		getline( cin, _preloadedBuffer );
-		if ( ! cin.good() ) {
+		getline( _in, _preloadedBuffer );
+		if ( ! _in.good() ) {
 			return nullptr;
 		}
 	}
@@ -632,17 +638,17 @@ void Replxx::ReplxxImpl::emulate_key_press( char32_t keyCode_ ) {
 char const* Replxx::ReplxxImpl::input( std::string const& prompt ) {
 	try {
 		errno = 0;
-		if ( ! tty::in ) { // input not from a terminal, we should work with piped input, i.e. redirected stdin
+		if ( ! tty::is_a_tty(_in_fd) ) { // input not from a terminal, we should work with piped input, i.e. redirected stdin
 			return ( read_from_stdin() );
 		}
 		if ( ! _errorMessage.empty() ) {
-			printf( "%s", _errorMessage.c_str() );
-			fflush( stdout );
+			dprintf( _out_fd, "%s", _errorMessage.c_str() );
+			fdatasync( _out_fd );
 			_errorMessage.clear();
 		}
 		if ( isUnsupportedTerm() ) {
-			fprintf( stdout, "%s", prompt.c_str() );
-			fflush( stdout );
+			dprintf( _out_fd, "%s", prompt.c_str() );
+			fdatasync( _out_fd );
 			return ( read_from_stdin() );
 		}
 		std::unique_lock<std::mutex> l( _mutex );
@@ -978,7 +984,7 @@ Replxx::ReplxxImpl::paren_info_t Replxx::ReplxxImpl::matching_paren( void ) {
 
 int Replxx::ReplxxImpl::virtual_render( char32_t const* buffer_, int len_, int& xPos_, int& yPos_, Prompt const* prompt_ ) {
 	Prompt const& prompt( prompt_ ? *prompt_ : _prompt );
-	return ( replxx::virtual_render( buffer_, len_, xPos_, yPos_, prompt.screen_columns(), _indentMultiline ? prompt.indentation() : 0 ) );
+	return ( replxx::virtual_render( buffer_, len_, xPos_, yPos_, prompt.screen_columns(), _indentMultiline ? prompt.indentation() : 0 ), _out_fd);
 }
 
 /**
@@ -1146,7 +1152,7 @@ char32_t Replxx::ReplxxImpl::do_complete_line( bool showCompletions_ ) {
 
 	// if no completions, we are done
 	if ( _completions.empty() ) {
-		beep();
+		beep(_err_fd);
 		return 0;
 	}
 
@@ -1165,7 +1171,7 @@ char32_t Replxx::ReplxxImpl::do_complete_line( bool showCompletions_ ) {
 		longestCommonPrefix = longest_common_prefix( _completions, ignoreCase );
 	}
 	if ( _beepOnAmbiguousCompletion && ( completionsCount != 1 ) ) { // beep if ambiguous
-		beep();
+		beep(_err_fd);
 	}
 
 	// if we can extend the item, extend it and return to main loop
@@ -1229,8 +1235,8 @@ char32_t Replxx::ReplxxImpl::do_complete_line( bool showCompletions_ ) {
 		_pos = _data.length();
 		refresh_line();
 		_pos = savePos;
-		printf( "\nDisplay all %u possibilities? (y or n)", static_cast<unsigned int>( _completions.size() ) );
-		fflush(stdout);
+		dprintf(_out_fd, "\nDisplay all %u possibilities? (y or n)", static_cast<unsigned int>( _completions.size() ) );
+		fdatasync(_out_fd);
 		onNewLine = true;
 		while (c != 'y' && c != 'Y' && c != 'n' && c != 'N' && c != Replxx::KEY::control('C')) {
 			do {
@@ -1278,15 +1284,15 @@ char32_t Replxx::ReplxxImpl::do_complete_line( bool showCompletions_ ) {
 		size_t rowCount = (_completions.size() + columnCount - 1) / columnCount;
 		for (size_t row = 0; row < rowCount; ++row) {
 			if (row == pauseRow) {
-				printf("\n--More--");
-				fflush(stdout);
+				dprintf(_out_fd, "\n--More--");
+				fdatasync(_out_fd);
 				c = 0;
 				bool doBeep = false;
 				while (c != ' ' && c != Replxx::KEY::ENTER && c != 'y' && c != 'Y' &&
 							 c != 'n' && c != 'N' && c != 'q' && c != 'Q' &&
 							 c != Replxx::KEY::control('C')) {
 					if (doBeep) {
-						beep();
+						beep(_err_fd);
 					}
 					doBeep = true;
 					do {
@@ -1297,18 +1303,18 @@ char32_t Replxx::ReplxxImpl::do_complete_line( bool showCompletions_ ) {
 					case ' ':
 					case 'y':
 					case 'Y':
-						printf("\r				\r");
+						dprintf(_out_fd, "\r				\r");
 						pauseRow += _terminal.get_screen_rows() - 1;
 						break;
 					case Replxx::KEY::ENTER:
-						printf("\r				\r");
+						dprintf(_out_fd, "\r				\r");
 						++pauseRow;
 						break;
 					case 'n':
 					case 'N':
 					case 'q':
 					case 'Q':
-						printf("\r				\r");
+						dprintf(_out_fd, "\r				\r");
 						stopList = true;
 						break;
 					case Replxx::KEY::control('C'):
@@ -1329,7 +1335,7 @@ char32_t Replxx::ReplxxImpl::do_complete_line( bool showCompletions_ ) {
 				if ( index < _completions.size() ) {
 					Completion const& c( _completions[index] );
 					int itemLength = static_cast<int>(c.text().length());
-					fflush(stdout);
+					fdatasync(_out_fd);
 
 					if ( longestCommonPrefix > 0 ) {
 						static UnicodeString const col( ansi_color( Replxx::Color::BRIGHTMAGENTA ) );
@@ -1353,13 +1359,13 @@ char32_t Replxx::ReplxxImpl::do_complete_line( bool showCompletions_ ) {
 
 					if ( ((column + 1) * rowCount) + row < _completions.size() ) {
 						for ( int k( itemLength ); k < longestCompletion; ++k ) {
-							printf( " " );
+							dprintf(_out_fd,  " " );
 						}
 					}
 				}
 			}
 		}
-		fflush(stdout);
+		fdatasync(_out_fd);
 	}
 
 	// display the prompt on a new line, then redisplay the input buffer
@@ -1474,7 +1480,7 @@ Replxx::ACTION_RESULT Replxx::ReplxxImpl::insert_character( char32_t c ) {
 	 * don't insert control characters
 	 */
 	if ( ( c >= static_cast<int>( Replxx::KEY::BASE ) ) || ( is_control_code( c ) && ( c != '\n' ) ) ) {
-		beep();
+		beep(_err_fd);
 		return ( Replxx::ACTION_RESULT::CONTINUE );
 	}
 	if ( ! _overwrite || ( _pos >= _data.length() ) ) {
@@ -1684,7 +1690,7 @@ Replxx::ACTION_RESULT Replxx::ReplxxImpl::yank( char32_t ) {
 		_killRing.lastAction = KillRing::actionYank;
 		_lastYankSize = restoredText->length();
 	} else {
-		beep();
+		beep(_err_fd);
 	}
 	return ( Replxx::ACTION_RESULT::CONTINUE );
 }
@@ -1692,12 +1698,12 @@ Replxx::ACTION_RESULT Replxx::ReplxxImpl::yank( char32_t ) {
 // meta-Y, "yank-pop", rotate popped text
 Replxx::ACTION_RESULT Replxx::ReplxxImpl::yank_cycle( char32_t ) {
 	if ( _killRing.lastAction != KillRing::actionYank ) {
-		beep();
+		beep(_err_fd);
 		return ( Replxx::ACTION_RESULT::CONTINUE );
 	}
 	UnicodeString* restoredText = _killRing.yankPop();
 	if ( !restoredText ) {
-		beep();
+		beep(_err_fd);
 		return ( Replxx::ACTION_RESULT::CONTINUE );
 	}
 	_pos -= _lastYankSize;
@@ -2314,7 +2320,7 @@ Replxx::ACTION_RESULT Replxx::ReplxxImpl::incremental_history_search( char32_t s
 					_history.restore_pos();
 					historyLinePosition = _pos;
 				} else {
-					beep();
+					beep(_err_fd);
 				}
 			} break;
 
@@ -2326,7 +2332,7 @@ Replxx::ACTION_RESULT Replxx::ReplxxImpl::incremental_history_search( char32_t s
 					dp._searchText.insert( dp._searchText.length(), c );
 					dp.updateSearchPrompt();
 				} else {
-					beep();
+					beep(_err_fd);
 				}
 			}
 		} // switch
@@ -2375,7 +2381,7 @@ Replxx::ACTION_RESULT Replxx::ReplxxImpl::incremental_history_search( char32_t s
 					lineSearchPos = ( dp._direction > 0 ) ? 0 : ( activeHistoryLine.length() - dp._searchText.length() );
 				} else {
 					historyLinePosition = _pos;
-					beep();
+					beep(_err_fd);
 					break;
 				}
 			} // while
@@ -2426,7 +2432,7 @@ Replxx::ACTION_RESULT Replxx::ReplxxImpl::bracketed_paste( char32_t ) {
 	static const UnicodeString BRACK_PASTE_SUFF( "\033[201~" );
 	static const int BRACK_PASTE_SLEN( BRACK_PASTE_SUFF.length() );
 	UnicodeString buf;
-	while ( char32_t c = read_unicode_character() ) {
+	while ( char32_t c = read_unicode_character(_in_fd) ) {
 		if ( ( c == '\r' ) || ( c == KEY::control( 'M' ) ) ) {
 			c = '\n';
 		}

src/replxx_impl.hxx
@@ -158,8 +158,14 @@ private:
 	bool _moveCursor;
 	bool _ignoreCase;
 	mutable std::mutex _mutex;
+
+	std::istream & _in;
+	[[ maybe_unused ]] std::ostream & _out;
+	int _in_fd = 0;
+	int _out_fd = 1;
+	int _err_fd = 2;
 public:
-	ReplxxImpl( FILE*, FILE*, FILE* );
+	ReplxxImpl( std::istream & in_, std::ostream & out_, int in_fd_, int out_fd_, int err_fd_ );
 	virtual ~ReplxxImpl( void );
 	void set_modify_callback( Replxx::modify_callback_t const& fn );
 	void set_completion_callback( Replxx::completion_callback_t const& fn );

src/terminal.cxx
@@ -65,8 +65,11 @@ bool is_a_tty( int fd_ ) {
 	return ( aTTY );
 }
 
+/**
+The patch to support arbitrary file descriptors made these functions unusable.
 bool in( is_a_tty( 0 ) );
 bool out( is_a_tty( 1 ) );
+*/
 
 }
 
@@ -81,7 +84,7 @@ static void WindowSizeChanged( int ) {
 #endif
 
 
-Terminal::Terminal( void )
+Terminal::Terminal(int in_fd_, int out_fd_)
 #ifdef _WIN32
 	: _consoleOut( INVALID_HANDLE_VALUE )
 	, _consoleIn( INVALID_HANDLE_VALUE )
@@ -99,7 +102,9 @@ Terminal::Terminal( void )
 	, _interrupt()
 #endif
 	, _rawMode( false )
-	, _utf8() {
+	, _utf8()
+	, _in_fd(in_fd_)
+	, _out_fd(out_fd_) {
 #ifdef _WIN32
 	_interrupt = CreateEvent( nullptr, true, false, TEXT( "replxx_interrupt_event" ) );
 #else
@@ -137,7 +142,7 @@ void Terminal::write8( char const* data_, int size_ ) {
 		disable_out();
 	}
 #else
-	int nWritten( write( 1, data_, size_ ) );
+	int nWritten( write( _out_fd, data_, size_ ) );
 #endif
 	if ( nWritten != size_ ) {
 		throw std::runtime_error( "write failed" );
@@ -153,7 +158,7 @@ int Terminal::get_screen_columns( void ) {
 	cols = inf.dwSize.X;
 #else
 	struct winsize ws;
-	cols = ( ioctl( 1, TIOCGWINSZ, &ws ) == -1 ) ? 80 : ws.ws_col;
+	cols = ( ioctl( _out_fd, TIOCGWINSZ, &ws ) == -1 ) ? 80 : ws.ws_col;
 #endif
 	// cols is 0 in certain circumstances like inside debugger, which creates
 	// further issues
@@ -168,7 +173,7 @@ int Terminal::get_screen_rows( void ) {
 	rows = 1 + inf.srWindow.Bottom - inf.srWindow.Top;
 #else
 	struct winsize ws;
-	rows = (ioctl(1, TIOCGWINSZ, &ws) == -1) ? 24 : ws.ws_row;
+	rows = (ioctl(_out_fd, TIOCGWINSZ, &ws) == -1) ? 24 : ws.ws_row;
 #endif
 	return (rows > 0) ? rows : 24;
 }
@@ -217,10 +222,10 @@ int Terminal::enable_raw_mode( void ) {
 	GetConsoleMode( _consoleIn, &_origInMode );
 #else
 
-	if ( ! tty::in ) {
+	if ( ! tty::is_a_tty(_in_fd) ) {
 		return ( notty() );
 	}
-	if ( tcgetattr( 0, &_origTermios ) == -1 ) {
+	if ( tcgetattr( _in_fd, &_origTermios ) == -1 ) {
 		return ( notty() );
 	}
 
@@ -263,14 +268,14 @@ int Terminal::reset_raw_mode( void ) {
 #ifdef _WIN32
 	SetConsoleMode(
 		_consoleIn,
-		( _origInMode & ~( ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT | ENABLE_PROCESSED_INPUT ) ) | ENABLE_QUICK_EDIT_MODE 
+		( _origInMode & ~( ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT | ENABLE_PROCESSED_INPUT ) ) | ENABLE_QUICK_EDIT_MODE
 	);
 	SetConsoleCP( 65001 );
 	enable_out();
 	return ( 0 );
 #else
 	/* put terminal in raw mode after flushing */
-	return ( tcsetattr( 0, TCSADRAIN, &_rawModeTermios ) );
+	return ( tcsetattr( _in_fd, TCSADRAIN, &_rawModeTermios ) );
 #endif
 }
 
@@ -285,7 +290,7 @@ void Terminal::disable_raw_mode(void) {
 	_consoleIn = INVALID_HANDLE_VALUE;
 #else
 	_terminal_ = nullptr;
-	if ( tcsetattr( 0, TCSADRAIN, &_origTermios ) == -1 ) {
+	if ( tcsetattr( _in_fd, TCSADRAIN, &_origTermios ) == -1 ) {
 		return;
 	}
 #endif
@@ -301,7 +306,7 @@ void Terminal::disable_raw_mode(void) {
  *
  * @return char32_t Unicode character
  */
-char32_t read_unicode_character(void) {
+char32_t read_unicode_character(int in_fd_) {
 	static char8_t utf8String[5];
 	static size_t utf8Count = 0;
 	while (true) {
@@ -310,7 +315,7 @@ char32_t read_unicode_character(void) {
 		/* Continue reading if interrupted by signal. */
 		ssize_t nread;
 		do {
-			nread = read( STDIN_FILENO, &c, 1 );
+			nread = read( in_fd_, &c, 1 );
 		} while ((nread == -1) && (errno == EINTR));
 
 		if (nread <= 0) return 0;
@@ -335,9 +340,9 @@ char32_t read_unicode_character(void) {
 
 #endif // #ifndef _WIN32
 
-void beep() {
-	fprintf(stderr, "\x7");	// ctrl-G == bell/beep
-	fflush(stderr);
+void beep(int fd_) {
+	dprintf(fd_, "\x7");	// ctrl-G == bell/beep
+	fdatasync(fd_);
 }
 
 // replxx_read_char -- read a keystroke or keychord from the keyboard, and translate it
@@ -488,7 +493,7 @@ char32_t Terminal::read_char( void ) {
 	}
 
 #else
-	c = read_unicode_character();
+	c = read_unicode_character(_in_fd);
 	if (c == 0) {
 		return 0;
 	}
@@ -502,7 +507,7 @@ char32_t Terminal::read_char( void ) {
 #ifdef __REPLXX_DEBUG__
 	if (c == ctrlChar('^')) {	// ctrl-^, special debug mode, prints all keys hit,
 														 // ctrl-C to get out
-		printf(
+		dprintf(_out_fd,
 				"\nEntering keyboard debugging mode (on ctrl-^), press ctrl-C to exit "
 				"this mode\n");
 		while (true) {
@@ -510,7 +515,7 @@ char32_t Terminal::read_char( void ) {
 			int ret = read(0, keys, 10);
 
 			if (ret <= 0) {
-				printf("\nret: %d\n", ret);
+				dprintf(_out_fd,"\nret: %d\n", ret);
 			}
 			for (int i = 0; i < ret; ++i) {
 				char32_t key = static_cast<char32_t>(keys[i]);
@@ -538,13 +543,13 @@ char32_t Terminal::read_char( void ) {
 					friendlyTextBuf[2] = 0;
 					friendlyTextPtr = friendlyTextBuf;
 				}
-				printf("%d x%02X (%s%s)	", key, key, prefixText, friendlyTextPtr);
+				dprintf(_out_fd,"%d x%02X (%s%s)	", key, key, prefixText, friendlyTextPtr);
 			}
-			printf("\x1b[1G\n");	// go to first column of new line
+			dprintf(_out_fd,"\x1b[1G\n");	// go to first column of new line
 
 			// drop out of this loop on ctrl-C
 			if (keys[0] == ctrlChar('C')) {
-				printf("Leaving keyboard debugging mode (on ctrl-C)\n");
+				dprintf(_out_fd,"Leaving keyboard debugging mode (on ctrl-C)\n");
 				fflush(stdout);
 				return -2;
 			}
@@ -552,7 +557,7 @@ char32_t Terminal::read_char( void ) {
 	}
 #endif // __REPLXX_DEBUG__
 
-	c = EscapeSequenceProcessing::doDispatch(c);
+	c = EscapeSequenceProcessing::doDispatch(_in_fd, _err_fd, c);
 	if ( is_control_code( c ) ) {
 		c = Replxx::KEY::control( control_to_human( c ) );
 	}
@@ -633,7 +638,7 @@ Terminal::EVENT_TYPE Terminal::wait_for_input( int long timeout_ ) {
 	int nfds( max( _interrupt[0], _interrupt[1] ) + 1 );
 	while ( true ) {
 		FD_ZERO( &fdSet );
-		FD_SET( 0, &fdSet );
+		FD_SET( _in_fd, &fdSet );
 		FD_SET( _interrupt[0], &fdSet );
 		timeval tv{ timeout_ / 1000, static_cast<suseconds_t>( ( timeout_ % 1000 ) * 1000 ) };
 		int err( select( nfds, &fdSet, nullptr, nullptr, timeout_ > 0 ? &tv : nullptr ) );
@@ -656,7 +661,7 @@ Terminal::EVENT_TYPE Terminal::wait_for_input( int long timeout_ ) {
 				return ( EVENT_TYPE::RESIZE );
 			}
 		}
-		if ( FD_ISSET( 0, &fdSet ) ) {
+		if ( FD_ISSET( _in_fd, &fdSet ) ) {
 			return ( EVENT_TYPE::KEY_PRESS );
 		}
 	}
@@ -682,10 +687,10 @@ void Terminal::clear_screen( CLEAR_SCREEN clearScreen_ ) {
 #endif
 		if ( clearScreen_ == CLEAR_SCREEN::WHOLE ) {
 			char const clearCode[] = "\033c\033[H\033[2J\033[0m";
-			static_cast<void>( write(1, clearCode, sizeof ( clearCode ) - 1) >= 0 );
+			static_cast<void>( write(_out_fd, clearCode, sizeof ( clearCode ) - 1) >= 0 );
 		} else {
 			char const clearCode[] = "\033[J";
-			static_cast<void>( write(1, clearCode, sizeof ( clearCode ) - 1) >= 0 );
+			static_cast<void>( write(_out_fd, clearCode, sizeof ( clearCode ) - 1) >= 0 );
 		}
 		return;
 #ifdef _WIN32
@@ -750,17 +755,17 @@ void Terminal::set_cursor_visible( bool ) {}
 #ifndef _WIN32
 int Terminal::read_verbatim( char32_t* buffer_, int size_ ) {
 	int len( 0 );
-	buffer_[len ++] = read_unicode_character();
-	int statusFlags( ::fcntl( STDIN_FILENO, F_GETFL, 0 ) );
-	::fcntl( STDIN_FILENO, F_SETFL, statusFlags | O_NONBLOCK );
+	buffer_[len ++] = read_unicode_character(_in_fd);
+	int statusFlags( ::fcntl( _in_fd, F_GETFL, 0 ) );
+	::fcntl( _in_fd, F_SETFL, statusFlags | O_NONBLOCK );
 	while ( len < size_ ) {
-		char32_t c( read_unicode_character() );
+		char32_t c( read_unicode_character(_in_fd) );
 		if ( c == 0 ) {
 			break;
 		}
 		buffer_[len ++] = c;
 	}
-	::fcntl( STDIN_FILENO, F_SETFL, statusFlags );
+	::fcntl( _in_fd, F_SETFL, statusFlags );
 	return ( len );
 }
 
@@ -778,4 +783,3 @@ int Terminal::install_window_change_handler( void ) {
 #endif
 
 }
-

commit cd05ac3d9059e3586820b59489791df2e89766ea

Fix standalone build of replxx examples
The standalone CMake build (`cd build && make -j`) has been broken since
2a70695 ("Support for custom descriptors (#26)") which switched `Replxx`
to fd-based construction but neither updated the examples nor restored
the C entry point:

- Restore the C `replxx_init` declaration in `replxx.h` and define it in
  `replxx.cxx` using `std::cin`/`std::cout` plus `STDIN_FILENO`/
  `STDOUT_FILENO`/`STDERR_FILENO`.
- Update `examples/cxx-api.cxx` to use the 5-arg `Replxx` constructor
  (the default ctor no longer exists), and disambiguate
  `set_highlighter_callback` between the original and the new
  `_with_pos` overload by wrapping the bind expression in
  `Replxx::highlighter_callback_t`.

Co-Authored-By: Claude Opus 4.7 (1M context) <noreply@anthropic.com>

examples/cxx-api.cxx
@@ -12,6 +12,15 @@
 #include <thread>
 #include <chrono>
 
+#ifndef _WIN32
+#include <unistd.h>
+#else
+#include <io.h>
+#define STDIN_FILENO  _fileno( stdin )
+#define STDOUT_FILENO _fileno( stdout )
+#define STDERR_FILENO _fileno( stderr )
+#endif
+
 #include "replxx.hxx"
 #include "util.h"
 
@@ -397,7 +406,7 @@ int main( int argc_, char** argv_ ) {
 	}
 
 	// init the repl
-	Replxx rx;
+	Replxx rx( std::cin, std::cout, STDIN_FILENO, STDOUT_FILENO, STDERR_FILENO );
 	Tick tick( rx, keys, tickMessages, promptFan );
 	rx.install_window_change_handler();
 
@@ -419,7 +428,7 @@ int main( int argc_, char** argv_ ) {
 	// set the callbacks
 	using namespace std::placeholders;
 	rx.set_completion_callback( std::bind( &hook_completion, _1, _2, cref( examples ), ignoreCase ) );
-	rx.set_highlighter_callback( std::bind( &hook_color, _1, _2, cref( regex_color ), cref( word_color ) ) );
+	rx.set_highlighter_callback( Replxx::highlighter_callback_t( std::bind( &hook_color, _1, _2, cref( regex_color ), cref( word_color ) ) ) );
 	rx.set_hint_callback( std::bind( &hook_hint, _1, _2, _3, cref( examples ), ignoreCase ) );
 	if ( promptInCallback ) {
 		rx.set_modify_callback( std::bind( &hook_modify, _1, _2, &rx ) );

include/replxx.h
@@ -224,7 +224,7 @@ typedef struct ReplxxHistoryEntryTag {
  *
  * \return Replxx library resource holder.
  */
-// REPLXX_IMPEXP Replxx* replxx_init( void );
+REPLXX_IMPEXP Replxx* replxx_init( void );
 
 /*! \brief Cleanup resources used by Replxx library.
  *

src/replxx.cxx
@@ -85,6 +85,7 @@
 
 #include <algorithm>
 #include <cstdarg>
+#include <iostream>
 
 #ifdef _WIN32
 
@@ -343,10 +344,10 @@ Replxx::Color rgb666( int red_, int green_, int blue_ ) {
 
 }
 
-// ::Replxx* replxx_init() {
-// 	typedef ::Replxx* replxx_data_t;
-// 	return ( reinterpret_cast<replxx_data_t>( new replxx::Replxx::ReplxxImpl( nullptr, nullptr, nullptr ) ) );
-// }
+::Replxx* replxx_init() {
+	typedef ::Replxx* replxx_data_t;
+	return ( reinterpret_cast<replxx_data_t>( new replxx::Replxx::ReplxxImpl( std::cin, std::cout, STDIN_FILENO, STDOUT_FILENO, STDERR_FILENO ) ) );
+}
 
 void replxx_end( ::Replxx* replxx_ ) {
 	delete reinterpret_cast<replxx::Replxx::ReplxxImpl*>( replxx_ );